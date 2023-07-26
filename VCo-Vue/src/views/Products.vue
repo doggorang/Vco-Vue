70e7544a261e9040686cc271cb779bc51d6084f4
Kevin Setiabudi <kevinsetiabudi20@gmail.com>
2023-07-26 23:08:38 +0700

    init popup modal

--- VCo-Vue/src/views/Products.vue
+++ VCo-Vue/src/views/Products.vue
@@ -5,7 +5,7 @@ export default {
             activeTab: 0,
             products: [
                 `
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-1" class="single-product">
                                 <div class="part-1">
                                         <ul>
@@ -22,7 +22,7 @@ export default {
                                 </div>
                         </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-2" class="single-product">
                                 <div class="part-1">
                                         <span class="discount">15% off</span>
@@ -39,7 +39,7 @@ export default {
                                 </div>
                         </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-3" class="single-product">
                                 <div class="part-1">
                                         <ul>
@@ -56,7 +56,7 @@ export default {
                                 </div>
                         </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-4" class="single-product">
                                 <div class="part-1">
                                         <span class="new">new</span>
@@ -74,7 +74,7 @@ export default {
                         </div>
                 </div>`,
                 `
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-1" class="single-product">
                                 <div class="part-1">
                                         <ul>
@@ -91,7 +91,7 @@ export default {
                                 </div>
                         </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-2" class="single-product">
                                 <div class="part-1">
                                         <span class="discount">15% off</span>
@@ -108,7 +108,7 @@ export default {
                                 </div>
                         </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-3" class="single-product">
                                 <div class="part-1">
                                         <ul>
@@ -125,7 +125,7 @@ export default {
                                 </div>
                         </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-4" class="single-product">
                                 <div class="part-1">
                                         <span class="new">new</span>
@@ -143,7 +143,7 @@ export default {
                         </div>
                 </div>`,
                 `
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-1" class="single-product">
                                 <div class="part-1">
                                         <ul>
@@ -160,7 +160,7 @@ export default {
                                 </div>
                         </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-2" class="single-product">
                                 <div class="part-1">
                                         <span class="discount">15% off</span>
@@ -177,7 +177,7 @@ export default {
                                 </div>
                         </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-3" class="single-product">
                                 <div class="part-1">
                                         <ul>
@@ -194,7 +194,7 @@ export default {
                                 </div>
                         </div>
                 </div>
-                <div class="col-md-6 col-lg-4 col-xl-3">
+                <div class="col-md-6 col-lg-4 col-xl-3" data-bs-toggle="modal" data-bs-target="#modalProduct">
                         <div id="product-4" class="single-product">
                                 <div class="part-1">
                                         <span class="new">new</span>
@@ -246,70 +246,89 @@ export default {
     <main class="page-content">
         <!-- Product List-->
         <section>
-            <div class="context-dark">
-            <div class="parallax-container">
-                <div class="parallax-content">
-                <div class="container section-66 section-md-110">
-                    <div>
-                    <h1 class="font-accent"><span class="big">Our Products</span></h1>
-                    </div>
-                    <ul class="list-inline list-inline-mg list-inline-squared h3 text-uppercase text-light offset-top-4 d-none d-sm-block">
-                    <li class="list-inline-item">A variety of great dishes</li>
-                    </ul>
+                <div class="context-dark">
+                <div class="parallax-container">
+                        <div class="parallax-content">
+                        <div class="container section-66 section-md-110">
+                        <div>
+                        <h1 class="font-accent"><span class="big">Our Products</span></h1>
+                        </div>
+                        <ul class="list-inline list-inline-mg list-inline-squared h3 text-uppercase text-light offset-top-4 d-none d-sm-block">
+                        <li class="list-inline-item">A variety of great dishes</li>
+                        </ul>
+                        </div>
+                        </div>
                 </div>
                 </div>
-            </div>
-            </div>
-            <div class="container container-wide section-66">
-                <div class="isotope-wrap">
-                    <div class="row">
-                        <!-- Isotope Filters-->
-                        <div class="col-xl-12">
-                            <div class="isotope-filters isotope-filters-horizontal">
-                                <ul class="list-inline list-inline-sm">
-                                    <li class="list-inline-item d-lg-none">
-                                    <p>Choose your category:</p>
-                                    </li>
-                                    <li class="list-inline-item section-relative">
-                                    <button class="isotope-filters-toggle btn btn-sm btn-default" data-custom-toggle="isotope-1" data-custom-toggle-disable-on-blur="true" @click="filterButton()">Filter<span class="caret"></span></button>
-                                    <div class="fd__list" v-show="showList">
-                                        <ul>
-                                                <li class="list-inline-item fd__item"><a class="text-sbold"
-                                                        :class="{ 'active': activeTab == 0 }" @click="filter('All')">All</a></li>
-                                                <li class="list-inline-item fd__item"><a class="text-sbold"
-                                                        :class="{ 'active': activeTab == 1 }" @click="filter('Veggetable')">Veggetable</a></li>
-                                                <li class="list-inline-item fd__item"><a class="text-sbold"
-                                                        :class="{ active: activeTab == 2 }" @click="filter('Snack')">Snack</a></li>
-                                                <li class="list-inline-item fd__item"><a class="text-sbold"
-                                                        :class="{ active: activeTab == 3 }" @click="filter('Dessert')">Dessert</a></li>
+                <div class="container container-wide section-66">
+                        <div class="isotope-wrap">
+                        <div class="row">
+                                <!-- Isotope Filters-->
+                                <div class="col-xl-12">
+                                <div class="isotope-filters isotope-filters-horizontal">
+                                        <ul class="list-inline list-inline-sm">
+                                        <li class="list-inline-item d-lg-none">
+                                        <p>Choose your category:</p>
+                                        </li>
+                                        <li class="list-inline-item section-relative">
+                                        <button class="isotope-filters-toggle btn btn-sm btn-default" data-custom-toggle="isotope-1" data-custom-toggle-disable-on-blur="true" @click="filterButton()">Filter<span class="caret"></span></button>
+                                        <div class="fd__list" v-show="showList">
+                                                <ul>
+                                                        <li class="list-inline-item fd__item"><a class="text-sbold"
+                                                                :class="{ 'active': activeTab == 0 }" @click="filter('All')">All</a></li>
+                                                        <li class="list-inline-item fd__item"><a class="text-sbold"
+                                                                :class="{ 'active': activeTab == 1 }" @click="filter('Veggetable')">Veggetable</a></li>
+                                                        <li class="list-inline-item fd__item"><a class="text-sbold"
+                                                                :class="{ active: activeTab == 2 }" @click="filter('Snack')">Snack</a></li>
+                                                        <li class="list-inline-item fd__item"><a class="text-sbold"
+                                                                :class="{ active: activeTab == 3 }" @click="filter('Dessert')">Dessert</a></li>
+                                                </ul>
+                                        </div>
+                                        <ul class="list-sm-inline isotope-filters-list" id="isotope-1">
+                                                <!-- .teamSelector { cursor: pointer; } -->
+                                                <!-- span:hover { cursor:pointer; } -->
+                                                <li class="list-inline-item"><a class="text-sbold"
+                                                :class="{ 'active': activeTab == 0 }" @click="filter('All')">All</a></li>
+                                                <li class="list-inline-item"><a class="text-sbold"
+                                                :class="{ 'active': activeTab == 1 }" @click="filter('Veggetable')">Veggetable</a></li>
+                                                <li class="list-inline-item"><a class="text-sbold"
+                                                :class="{ active: activeTab == 2 }" @click="filter('Snack')">Snack</a></li>
+                                                <li class="list-inline-item"><a class="text-sbold"
+                                                :class="{ active: activeTab == 3 }" @click="filter('Dessert')">Dessert</a></li>
                                         </ul>
-                                    </div>
-                                    <ul class="list-sm-inline isotope-filters-list" id="isotope-1">
-                                        <!-- .teamSelector { cursor: pointer; } -->
-                                        <!-- span:hover { cursor:pointer; } -->
-                                        <li class="list-inline-item"><a class="text-sbold"
-                                            :class="{ 'active': activeTab == 0 }" @click="filter('All')">All</a></li>
-                                        <li class="list-inline-item"><a class="text-sbold"
-                                            :class="{ 'active': activeTab == 1 }" @click="filter('Veggetable')">Veggetable</a></li>
-                                        <li class="list-inline-item"><a class="text-sbold"
-                                            :class="{ active: activeTab == 2 }" @click="filter('Snack')">Snack</a></li>
-                                        <li class="list-inline-item"><a class="text-sbold"
-                                            :class="{ active: activeTab == 3 }" @click="filter('Dessert')">Dessert</a></li>
-                                    </ul>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-xl-12 offset-top-34">
-                            <section class="section-products">
-                                <div class="container">
-                                    <div class="row" v-html="content"></div>
+                                        </li>
+                                        </ul>
                                 </div>
-                            </section>
+                                </div>
+                                <div class="col-xl-12 offset-top-34">
+                                <section class="section-products">
+                                        <div class="container">
+                                        <div class="row" v-html="content"></div>
+                                        </div>
+                                </section>
+                                </div>
                         </div>
-                    </div>
+                        </div>
                 </div>
-            </div>
+
+                <!-- Modal -->
+                <div class="modal fade" id="modalProduct" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                                <div class="modal-content">
+                                        <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                                ...
+                                        </div>
+                                        <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
         </section>
     </main>
 </template>
